84b30a7a5b048c1967b3f8dddb99e2bfc76c4571
Pooya Shirazi <p.shirazi.a@gmail.com>
2025-02-26 22:26:26 +0330

    ...

--- app/bot.py
+++ app/bot.py
@@ -1,8 +1,13 @@
 import telebot
 from flask_sqlalchemy import SQLAlchemy
+import schedule
+import time
+from threading import Thread
 
 TELEGRAM_TOKEN = '8043272038:AAFbx_iyOmTSco0U_5pG7a-51NjZ7uLWcaU'
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
+CHAT_ID = -4638690648
+
 
 def init_bot(app):
     
@@ -20,19 +25,16 @@ def init_bot(app):
                 parse_mode="HTML",
             )
     
-# @bot.message_handler(func=lambda message: True)
-# def get_group_chat_id(message):
-#     chat_id = message.chat.id
-#     bot.reply_to(message, f"This group's chat ID is: {chat_id}")
+
+
 
 def send_new_register(text, file_path=None):
-    
-    CHAT_ID = -4638690648
-    
+
     bot.send_message(
         chat_id=CHAT_ID,
         text=text,
-        parse_mode="HTML")
+        parse_mode="HTML"
+    )
     
     if file_path:
         print(file_path)
@@ -45,5 +47,30 @@ def send_new_register(text, file_path=None):
             )
 
 
+def send_daily_message():    
+    from app.database.models import Register
+    from app import create_app
+    app = create_app()
+    with app.app_context():
+        registrations = Register.query.count()
+        message = f"این یک پیام خودکار است.\nتعداد افراد ثبت نام شده: {registrations} نفر."
+        bot.send_message(
+            chat_id=CHAT_ID,
+            text=message,
+            parse_mode="HTML"
+        )
+
+def start_scheduler():
+    schedule.every().day.at("21:00").do(send_daily_message)
+    schedule.every().day.at("9:00").do(send_daily_message)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+
 def start_bot():
+    scheduler_thread = Thread(target=start_scheduler)
+    scheduler_thread.daemon = True
+    scheduler_thread.start()
     bot.polling()
