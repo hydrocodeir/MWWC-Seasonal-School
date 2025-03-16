d25bcd892a9ebee8c2a97dd2efd973beb1412011
Pooya Shirazi <p.shirazi.a@gmail.com>
2025-03-16 22:21:15 +0330

    ...

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -4,4 +4,9 @@ $(document).ready(function () {
     jalaliDatepicker.startWatch();
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    const welcomeModal = new bootstrap.Modal(document.getElementById('welcomeModal'));
+    welcomeModal.show();
+});
+
 
